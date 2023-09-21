38093109aef5207035da304ba3b379da05572c0b
Yagavi <yagavi072002@gmail.com>
2023-09-21 12:39:01 +0530

    commit message here

---
+++ models/user.ts
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    uID : {
+        type: String,
+        unique: true,
+        required: true
+    },
+    uName: {
+        type: String,
+        required: true
+    },
+    uEmail: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    uPassword: {
+        type: String,
+        required: true
+    },
+    uCatalog: {
+        type: [String]
+    }
+})
+
+const User = mongoose.model('User',userSchema)
+
+export { User }
