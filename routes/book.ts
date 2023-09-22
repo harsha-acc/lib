a1534b76f417a53356f277c69b7fd3e53b12fb85
Aakkash24 <skybully07@gmail.com>
2023-09-22 17:08:23 +0530

    Temp

--- routes/book.ts
+++ routes/book.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { createBook, deleteBook, readBook } from "../controllers/book";
+import { createBook, deleteBook, readBook, updateBook } from "../controllers/book";
 
 const bookRouter: Router = Router();
 bookRouter.post("/create",createBook);
 bookRouter.delete("/:bID",deleteBook);
 bookRouter.get('/all', readBook);
-// bookRouter.post("/update",updateBook);
+bookRouter.post("/update",updateBook);
 
 export {bookRouter}
