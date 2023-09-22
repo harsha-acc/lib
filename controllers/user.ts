eb6361b8c495330cbc2cd5d4a0ad9f910ce84325
Harsha <balasaharsha@gmail.com>
2023-09-22 10:23:36 +0530

    Ft: Bcrypt integration

--- controllers/user.ts
+++ controllers/user.ts
@@ -1,11 +1,17 @@
 import { Request, Response } from "express"
 import { User } from '../models/user'
+import bcrypt from 'bcrypt'
 
-const userLogin = (req:Request,res:Response)=>{
+
+const SALT_ROUNDS: number = 10
+
+const userLogin = (req: Request, res: Response)=>{
     res.send("User Login called")
 }
 
-const userSignUp = (req: Request, res: Response) => {
+const userSignUp = async (req: Request, res: Response) => {
+    req.body.uPassword = await bcrypt.hash(req.body.uPassword, SALT_ROUNDS)
+    console.log(req.body)
     const newUser = new User(req.body)
     newUser.save().then(() => {
         res.json({ message: "Created successfully" })
