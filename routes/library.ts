517b3ed733584ad58cd2ad4aa5524d7218703afb
Harsha <balasaharsha@gmail.com>
2023-09-26 16:08:02 +0530

    Ft: New Library API integration

--- routes/library.ts
+++ routes/library.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { libraryLogin, librarySignUp } from "../controllers/library";
+import { libraryAll, libraryLogin, librarySignUp } from "../controllers/library";
 
 const libraryRouter: Router = Router();
 libraryRouter.post("/login", libraryLogin);
 libraryRouter.post("/signup", librarySignUp);
+libraryRouter.get('/all', libraryAll)
 
 export { libraryRouter }
